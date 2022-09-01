8d591c27e4fdd678dfc2c741136f6b2a17ef4032
Marc Towler <marc@marctowler.co.uk>
2022-09-01 23:06:42 +0100

    Added basic saving system

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,32 @@
+﻿using Omniworlds.Scripts.Saving;
+using UnityEngine;
+
+namespace Omniworlds.Scripts.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string DefaultSaveFile = "save";
+        
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            else if(Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+        
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(DefaultSaveFile);
+        }
+        
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(DefaultSaveFile);
+        }
+    }
+}
